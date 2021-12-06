baa1c4af11495a144aa525cc6c814f0d6eff8431
jxhuang0508 <55238474+jxhuang0508@users.noreply.github.com>
2021-12-06 19:07:49 +0800

    Update test.sh

--- hcl_target/test.sh
+++ hcl_target/test.sh
@@ -1,6 +1,6 @@
 for i in {300..10000000..300}
 do
 	echo "TEST $i MODEL"
-	python evaluate_cityscapes_advent_best.py --restore-from ./snapshots/HCL_historical_momentum_scaleaug_HCID/GTA5_$i.pth
+	python evaluate_cityscapes_advent_best.py --restore-from ./snapshots/HCL_target/GTA5_$i.pth
 done
 
